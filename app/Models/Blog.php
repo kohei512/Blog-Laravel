8b95de7b5f110e8b8490d1fe2fe24fc323cba297
kohei512 <cae35246@gmail.com>
2021-01-12 12:22:31 +0900

    blog

---
+++ app/Models/Blog.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Blog extends Model
+{
+
+
+    //テーブル名
+    protected $table = 'blogs';
+
+    //可変項目
+    protected $fillable =
+    [
+        'title',
+        'content'
+    ];
+
+
+}
+
+// class Blog extends Model
+// {
+//     use HasFactory;
+// }
